3bc16a2a33f3417d5b15033584b6874967ed25e3
kspl456 <146167786+kspl456@users.noreply.github.com>
2025-06-30 22:37:45 +0530

    Update Rotate-Array.py

--- Arrays/Rotate-Array.py
+++ Arrays/Rotate-Array.py
@@ -15,11 +15,6 @@ def rotate(nums, k):
         reverse(nums,0,k-1)
         reverse(nums,k,n-1)
 
-'''
-Examples:
-Input: nums = [1, 2, 3, 4, 5], k = 2
-Output: [4, 5, 1, 2, 3]
-
-Input: nums = [10, 20, 30, 40, 50], k = 3
-Output: [30, 40, 50, 10, 20]
-'''
+# Example:
+# Input: nums = [1, 2, 3, 4, 5], k = 2
+# Output: [4, 5, 1, 2, 3]
